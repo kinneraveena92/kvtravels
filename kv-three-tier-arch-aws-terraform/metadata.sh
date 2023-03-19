5c1b67042d1ea93edf54d4c0720c29edf2e10d95
kinneraveena <kinnera.veena@xylem.com>
2023-03-19 18:31:44 +0530

    code for 3 tier architecture

--- kv-three-tier-arch-aws-terraform/metadata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-curl -sL http://169.254.169.254/latest/meta-data >> temp.txt
-file="temp.txt"
-while read -r line; do
-echo -e "$line\n"
-variable=$(curl -sL http://169.254.169.254/latest/meta-data/$line)
-echo "$variable"
-done
